1500747a62fbc94163b993c8950c43dc405361c0
ZCP1 <zcp1@ZCP1s-MacBook-Pro.local>
2024-03-24 10:58:37 -0700

    together working finished

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,21 +1,17 @@
 import { useState } from "react";
-import { useAutoCompleteSearchResults } from "../CustomHooks/hooks";
-import { searchHander } from "../Functions/functions";
 
 export function Search({ breeds, setSearchedBreed }) {
-    const [input, setInput] = useState('');
     const [result, setResult] = useState('Affenpinscher');
-    const [searchedResults] = useAutoCompleteSearchResults(breeds, input)
+    const searchHander = (e, breedObj, setSearchedBreed, setResult) => {
+        e.preventDefault()
+        setSearchedBreed([breedObj.name, breedObj.id])
+        setResult(breedObj.name)
+    }
 
     return (
         <div id="search">
-            <h3>Search for the Breed you want to know about</h3>
-            <h4>Result for <span id="result">{result}</span></h4>
-            <form>
-                <input id="search-input" value={input} placeholder="Search here..." onChange={e => setInput(e.target.value)} />
-            </form>
             <ul id="search-results">
-                { searchedResults.length !== 0 && searchedResults.map(breedName => <li key={breedName} className="breedName" onClick={(e) => searchHander(e, breedName, setSearchedBreed, setResult)}>{breedName[0]}</li>) }
+                { breeds.length !== 0 && breeds.map(breedObj => <li key={breedObj.name} className="breedName" onClick={(e) => searchHander(e, breedObj, setSearchedBreed, setResult)}>{breedObj.name}</li>) }
             </ul> 
         </div>
     )
